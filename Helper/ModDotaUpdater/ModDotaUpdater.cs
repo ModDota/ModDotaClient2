81534b4f16dc1cf4a48dce0ecfb4fa7a41eac563
Derek Morris <dmorris321@gmail.com>
2015-02-18 23:03:14 -0500

    Change the updater to be solely based on renaming, as per the new update scheme.

--- Helper/ModDotaUpdater/ModDotaUpdater.cs
+++ Helper/ModDotaUpdater/ModDotaUpdater.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Reflection;
@@ -24,51 +25,17 @@ namespace ModDotaUpdater
             Thread.Sleep(5000);
             // All this does is check ModDotaHelper for updates, then exits
             // This is due to the inability to update your own executable cleanly in a cross-platform manner
-            Version sug = GetSuggestedVersion();
-            Version ins = GetInstalledVersion();
-            if (ins < sug)
-            {
-                using (WebClient client = new WebClient())
-                {
-                    client.DownloadFile("https://moddota.com/mdc/ModDotaHelper"+sug.ToString()+".exe", "ModDotaHelper.exe");
-                }
-            }
-            // Start the helper back up
-            Process.Start("ModDotaHelper.exe");
-            // So long and thanks for all the fish
-            Process.GetCurrentProcess().Kill();
-        }
-        /// <summary>
-        /// Get the version of the Helper suggested by ModDota
-        /// </summary>
-        /// <returns>A Version object pulled from the version page</returns>
-        static Version GetSuggestedVersion()
-        {
             try
             {
-                using (WebClient client = new WebClient())
+                if (File.Exists("ModDotaHelper.exe.new"))
                 {
-                    string s = client.DownloadString("https://moddota.com/mdc/ModDotaHelper.version");
-                    return new Version(s);
+                    File.Move("ModDotaHelper.exe.new", "ModDotaHelper.exe");
                 }
-            } catch (Exception)
-            {
-                return new Version();
             }
-        }
-        /// <summary>
-        /// Get the version of the currently installed version of the Helper
-        /// </summary>
-        /// <returns>The Version contained inside the assembly of ModDotaHelper</returns>
-        static Version GetInstalledVersion()
-        {
-            try
+            finally
             {
-                return AssemblyName.GetAssemblyName("ModDotaHelper.exe").Version;
-            } catch (Exception)
-            {
-                // can't open it, so force update
-                return new Version();
+                // Start the helper back up
+                Process.Start("ModDotaHelper.exe");
             }
         }
     }
